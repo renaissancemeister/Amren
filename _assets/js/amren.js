862e9f97b9f831df32a8c1d6881ce612fdac162f
Kevin McCormick <admin@nc-f.org>
2019-12-10 07:12:39 -0800

    fix AR index and site search
    and external BS.js link update

--- _assets/js/amren.js
+++ _assets/js/amren.js
@@ -1,5 +1,9 @@
-$(document).ready(function () {
-// load the ad columns
-    $('#right').html('Loading ...').load('/_includes/right-ads.html');
-    $('#left').html('Loading ...').load('/_includes/left-ads.html');
+$(document).ready(function() {
+  // load the ad columns
+  $("#right")
+    .html("Loading ...")
+    .load("/_includes/right-ads.html");
+  $("#left")
+    .html("Loading ...")
+    .load("/_includes/left-ads.html");
 });
--- _assets/js/serve.js
+++
@@ -1,8 +0,0 @@
-var referer="";try{if(referer=document.referrer,"undefined"==typeof referer)throw"undefined"}catch(exception){referer=document.location.href,(""==referer||"undefined"==typeof referer)&&(referer=document.URL)}referer=referer.substr(0,700);
-    var rcel = document.createElement("script");
-    rcel.id = 'rc_' + Math.floor(Math.random() * 1000);
-    rcel.type = 'text/javascript';
-    rcel.src = "http://trends.revcontent.com/serve.js.php?w=50827&t="+rcel.id+"&c="+(new Date()).getTime()+"&width="+(window.outerWidth || document.documentElement.clientWidth)+"&referer="+referer;
-    rcel.async = true;
-    var parent_div = document.querySelector('script[id=""]').parentNode;
-    parent_div.appendChild(rcel);
